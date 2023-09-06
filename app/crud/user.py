5032fd7fe6271fb5154e3f12329feaafff3cff9e
harshitsaini <harshitsaini15@gmail.com>
2023-09-06 17:00:18 +0530

    Minor session based improvements and updated auth token generation algo

--- app/crud/user.py
+++ app/crud/user.py
@@ -43,7 +43,7 @@ def create_user(db: Session, user: schemas.UserCreate):
     )
     db.add(db_user)
     db.commit()
-    db.refresh(db_user)
+    # db.refresh(db_user)
     return db_user
 
 
--- app/utils/security.py
+++ app/utils/security.py
@@ -9,7 +9,10 @@ from sqlalchemy.orm import Session
 
 
 from cryptography.hazmat.primitives import serialization
+
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.backends import default_backend
 from authlib.jose import JsonWebEncryption
 
 from .. import crud
@@ -17,20 +20,30 @@ from ..database import get_db
 from ..utils.otel import tracer
 
 
-def get_session_private_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
+def get_session_private_key(alg="ec"):
+    if alg == "ec":
+        # Generate ECDH key pair
+        private_key = ec.generate_private_key(ec.SECP521R1())
 
-    # Serialize the private key in PEM format
-    private_key_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
+        # Serialize keys to PEM format
+        private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+    else:
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        # Serialize the private key in PEM format
+        private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
 
-    session_private_key = private_key_pem.decode()
+    session_private_key = private_pem.decode()
     return session_private_key
 
 
@@ -39,6 +52,7 @@ def get_session_public_key(session_private_key):
     private_key = serialization.load_pem_private_key(
         session_private_key.encode(),
         password=None,  # Replace with the password if the private key is encrypted
+        backend=default_backend(),
     )
 
     # Extract the public key from the private key
@@ -51,15 +65,38 @@ def get_session_public_key(session_private_key):
     )
 
     session_public_key = public_key_pem.decode()
-
     return session_public_key
 
 
+# def get_session_token(
+#     user_id, session_id, client_id, platform_client_id, session_private_key
+# ):
+#     jwe = JsonWebEncryption()
+#     protected = {"alg": "RSA-OAEP-256", "enc": "A256GCM"}
+#     payload = json.dumps(
+#         {
+#             "userId": user_id,
+#             "sessionId": session_id,
+#             "clientId": client_id,
+#             "platformClientId": platform_client_id,
+#             "tier": "pro",
+#         }
+#     )
+
+#     session_public_key = get_session_public_key(session_private_key)
+#     session_token = jwe.serialize_compact(protected, payload, session_public_key)
+#     return session_token.decode()
+
+
 def get_session_token(
-    user_id, session_id, client_id, platform_client_id, session_private_key
+    user_id, session_id, client_id, platform_client_id, session_private_key, alg="ec"
 ):
     jwe = JsonWebEncryption()
-    protected = {"alg": "RSA-OAEP-256", "enc": "A256GCM"}
+    if alg == "ec":
+        protected = {"alg": "ECDH-ES+A256KW", "enc": "A256GCM"}
+    else:
+        protected = {"alg": "RSA-AEP", "enc": "A256GCM"}
+
     payload = json.dumps(
         {
             "userId": user_id,
@@ -155,7 +192,6 @@ async def get_current_user(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail=f"Invalid authentication credentials: {e}",
             )
-
         else:
             with tracer.start_as_current_span("get_user"):
                 current_user = crud.get_user(db, user_id=user_id)
